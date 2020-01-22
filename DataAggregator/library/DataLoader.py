352c18f3c140d97541ab7935b79a48c00594c48a
SergeyMerkudinov <mca97@yandex.ru>
2020-01-22 21:06:46 +0300

    add base files

--- DataAggregator/library/DataLoader.py
+++ DataAggregator/library/DataLoader.py
@@ -1,6 +1,9 @@
 import csv
 import os
 import codecs
+import json
+import urllib.request
+from urllib.error import URLError
 
 from DataAggregator.apps import DataaggregatorConfig
 from DataAggregator.models import Coach
@@ -47,4 +50,30 @@ class DataLoader:
                             i += 1
             except FileNotFoundError:
                 print("Unable to load CSV file in repository. Check AppSettings for the correct path")
+
+        if self.loadMethod == 'API':
+            # Firstly we need to call api to retrieve data
+            try:
+                apiUrl = DataaggregatorConfig.DATA_LOAD_API_PARAMS['PATH'] + '?' + '$top=' + str(DataaggregatorConfig.DATA_LOAD_MAX_RECORDS) + '&api_key=' + DataaggregatorConfig.DATA_LOAD_API_PARAMS['API_KEY']
+                response = urllib.request.urlopen(apiUrl).read().decode('utf-8')
+                response = json.loads(response)
+                for currentCoach in response:
+                    result.append(Coach(
+                        id=currentCoach['Number'],
+                        firstName=currentCoach['Cells']['Name'],
+                        lastName=currentCoach['Cells']['LastName'],
+                        secondName=currentCoach['Cells']['MiddleName'],
+                        gender=currentCoach['Cells']['Gender'],
+                        birthDate=currentCoach['Cells']['DateOfBirth'],
+                        sport=currentCoach['Cells']['Sport'][0]['SportName'],
+                        seniorityPeriod=currentCoach['Cells']['Sport'][0]['SeniorityPeriod'],
+                        citizenship=currentCoach['Cells']['Citizenship'][0]['Citizenship'],
+                        publicPhone=currentCoach['Cells']['PublicPhone'][0]['PublicPhone'],
+                        email=currentCoach['Cells']['Email'][0]['Email'],
+                        jobInfo=currentCoach['Cells']['JobOrganisation'][0]['JobOrganizationName']
+                    ))
+
+            except URLError:
+                print("Unable to connect and retrieve data from an external system")
+
         return result
